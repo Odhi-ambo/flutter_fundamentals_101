669d7f1b452b6d56b3ad782a78b8041c52af8d69
Odhi-ambo <michaelderrick605@gmail.com>
2024-05-02 18:38:35 +0300

    initial commit-Listview

--- lib/main.dart
+++ lib/main.dart
@@ -12,30 +12,22 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.amber,
-        appBar: AppBar(
-          title: Text("My AppBar"),
-          backgroundColor: Colors.blueAccent[200],
-          elevation: 0,
-          leading: Icon(Icons.menu),
-          actions: [IconButton(onPressed: () {}, icon: Icon(Icons.logout))],
-        ),
-        body: Center(
-          child: Container(
-            height: 200,
-            width: 200,
-            decoration: BoxDecoration(
-                color: Colors.blue, borderRadius: BorderRadius.circular(20)),
-
-            padding: EdgeInsets.symmetric(horizontal: 25, vertical: 25),
-
-            // padding: EdgeInsets.only(top: 10, left: 20),
-            child: Icon(
-              Icons.favorite,
-              color: Colors.white,
-              size: 32,
+        body: ListView(
+          scrollDirection: Axis.horizontal,
+          children: [
+            Container(
+              width: 350,
+              color: Colors.black12,
             ),
-          ),
+            Container(
+              width: 350,
+              color: Colors.amber,
+            ),
+            Container(
+              width: 350,
+              color: Colors.blueGrey,
+            )
+          ],
         ),
       ),
     );
