0849f5c475b7d78d5e997a4fd30ed576c9106c89
Odhi-ambo <michaelderrick605@gmail.com>
2024-05-06 10:10:18 +0300

    intial commit

--- lib/pages/first_page.dart
+++ lib/pages/first_page.dart
@@ -7,9 +7,16 @@ class FirstPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          title: Text(
-        'Fist page',
-      )),
+          backgroundColor: Colors.brown[400],
+          title: Center(
+            child: Text(
+              'First page',
+              style: TextStyle(
+                fontSize: 21,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          )),
       drawer: Drawer(
         backgroundColor: Colors.brown[400],
         child: Column(
@@ -23,14 +30,24 @@ class FirstPage extends StatelessWidget {
             ListTile(
               leading: Icon(Icons.home),
               title: Text('Home'),
+              onTap: () {
+                //go to homepage
+                Navigator.pushNamed(context, '/homepage');
+              },
             ),
             ListTile(
               leading: Icon(Icons.settings),
               title: Text('Settings'),
+              onTap: () {
+                Navigator.pushNamed(context, '/settingspage');
+              },
             ),
             ListTile(
               leading: Icon(Icons.account_circle),
               title: Text('Account'),
+              onTap: () {
+                Navigator.pushNamed(context, '/accountspage');
+              },
             ),
           ],
         ),
